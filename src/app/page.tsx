35aa9dbee3069e402aff6597b386fd6402994f2c
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-20 22:46:04 +0900

    レイアウト変更。ナビバーと左メニューを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,23 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-      <div>
+    <div>
+      <div className="flex flex-row p-2 m-1">
         <p>home</p>
-        <Link
-          href="/fontsize"
-          className=""
-        >
-          fontsize
-        </Link>
       </div>
+      <div className="flex flex-row p-2 m-1">
+        <div className="basis-1/4 p-2 m-1">
+          <Link 
+            href="/fontsize"
+          >
+            fontsize
+          </Link>
+          <br />
+        </div>
+        <div className="basis-3/4 p-2 m-1">
+        <p>aaa</p>
+        </div>
+      </div>
+    </div>
   )
 }
